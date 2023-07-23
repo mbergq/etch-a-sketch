45ff63a6a1dc4c23fabf02f1f42701dfcb0d1537
Martin <106612709+menkodot@users.noreply.github.com>
2023-07-23 12:41:21 +0200

    added gridbox with first square

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector('#container');
-const gridWrapper = document.getElementById('#gridWrapper');
+const gridWrapper = document.querySelector('#gridWrapper');
 
 
+//Noting that code below did not work unless i used "querySelector-above"
+const square = document.createElement('div');
+square.classList.add('square');
+gridWrapper.appendChild(square);
