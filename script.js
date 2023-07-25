1df317325913dba6358e1ce93dcf6c3e6bce02b1
Martin <106612709+menkodot@users.noreply.github.com>
2023-07-25 20:12:45 +0200

    grid-generator function added

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ const gridWrapper = document.querySelector('#gridWrapper');
 
 
 //Noting that code below did not work unless i used "querySelector-above"
-const square = document.createElement('div');
-square.classList.add('square');
-gridWrapper.appendChild(square);
+//const square = document.createElement('div');
+//square.classList.add('square');
+//gridWrapper.appendChild(square);
+
+
+//Multiple box-generator code
+var boxes = ["","","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",
+"bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx","bx",];
+
+boxes.forEach(function(el) {
+    var div = document.createElement('div');
+    div.className = "square";
+    div.innerHTML = el;
+    gridWrapper.appendChild(div);
+
+})
