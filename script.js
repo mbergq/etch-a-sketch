328c630dbf740f28b137758477204c1894e3da7b
Martin <106612709+menkodot@users.noreply.github.com>
2023-08-06 15:50:08 +0200

    colorFiller function added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container = document.querySelector('#container');
 const gridWrapper = document.querySelector('#gridWrapper');
 
-
 //Noting that code below did not work unless i used "querySelector-above"
 //const square = document.createElement('div');
 //square.classList.add('square');
@@ -9,7 +8,7 @@ const gridWrapper = document.querySelector('#gridWrapper');
 
 
 //Multiple box-generator code
-var boxes = new Array(1024).fill(['']).flat();
+let boxes = new Array(1024).fill(['']).flat();
 
 boxes.forEach(function(el) {
     var div = document.createElement('div');
@@ -19,3 +18,12 @@ boxes.forEach(function(el) {
 
 });
 
+let squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', function colorFiller(event) {
+        console.log('mouse hovered', event);
+
+        square.setAttribute('style', 'background-color: black;');
+    });
+})
